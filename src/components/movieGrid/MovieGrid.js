3c2e5dcb5c16a2d01a655aad0e4e82f4868cf660
abbiagarin <abbiagarin7@gmail.com>
2023-06-06 17:35:36 +0100

    work on the movie UI

--- src/components/movieGrid/MovieGrid.js
+++ src/components/movieGrid/MovieGrid.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { unavailable } from "../../Config/Config";
 import { MdOutlineFavoriteBorder, MdOutlineFavorite } from "react-icons/md";
-
+import MovieCard from "../UI/MovieCard";
 import "./MovieGrid.scss";
 
 const MovieGrid = ({ id, image, title, year, rating }) => {
@@ -11,20 +11,26 @@ const MovieGrid = ({ id, image, title, year, rating }) => {
     setFavourite(!favorite);
   };
   return (
-    <div className="movie__grid wrap">
-      <img src={image ? image : unavailable} alt="" className="movie_poster" />
+    <MovieCard>
+      <div className="movie__grid">
+        <img
+          src={image ? image : unavailable}
+          alt=""
+          className="movie_poster"
+        />
 
-      <div className="movie__grid-subTitle text ">
-        <b className="movie__grid-title">{title}</b>
-        <b>
-          <span onClick={handleFavorite}>
-            {favorite ? <MdOutlineFavorite /> : <MdOutlineFavoriteBorder />}
-          </span>
-          {year}
-        </b>
-        <b>{rating}</b>
+        <div className="movie__grid-subTitle text ">
+          <b className="movie__grid-title">{title}</b>
+          <b>
+            <span onClick={handleFavorite}>
+              {favorite ? <MdOutlineFavorite /> : <MdOutlineFavoriteBorder />}
+            </span>
+            {year}
+          </b>
+          <b>{rating}</b>
+        </div>
       </div>
-    </div>
+    </MovieCard>
   );
 };
 
